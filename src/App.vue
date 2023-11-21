a61d646e036d63218d8846d6919bd870f2bd6155
snowin-jj <snowin.developer@gmail.com>
2023-11-21 19:22:42 +0530

    add auto update support

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-import { platform } from '@tauri-apps/api/os'
 
 import { useStateStore } from './store'
 import Timer from './components/Timer.vue';
@@ -9,19 +7,12 @@ import TimeForm from './components/TimeForm.vue';
 import AppTitleBar from './components/AppTitleBar.vue';
 
 
-const bgColor = ref<"bg-transparent" | "bg-[#121212]">("bg-transparent")
 const store = useStateStore()
-
-onMounted(async () => {
-  const color = await platform() === "win32" ? "bg-[#121212]" : "bg-transparent"
-  bgColor.value = color
-})
-
 
 </script>
 
 <template>
-  <main data-tauri-drag-region class="min-h-[100dvh] grid place-items-center" :class="bgColor">
+  <main data-tauri-drag-region class="min-h-[100dvh] grid place-items-center">
     <AppTitleBar />
     <Timer v-if="store.state === 'running' || store.state === 'paused'" />
     <BreakMenu v-else-if="store.state === 'break'" />
